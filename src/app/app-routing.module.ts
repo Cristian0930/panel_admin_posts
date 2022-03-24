10ac15c0566e5c2bc493088941deed0fab296c88
Cristian López Ramirez <cl827209@gmail.com>
2022-03-24 11:53:41 -0600

    añadir bootstrap 5 y crear componentes a utilizar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component';
+import { SiginComponent } from './components/sigin/sigin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'signin', pathMatch: 'full'
+  },
+  {
+    path: 'posts', component: PostsComponent
+  },
+  {
+    path: 'signup', component: SignupComponent
+  },
+  {
+    path: 'signin', component: SiginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SiginComponent } from './components/sigin/sigin.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SiginComponent,
+    PostsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
